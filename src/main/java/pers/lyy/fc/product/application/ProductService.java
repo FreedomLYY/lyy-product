c4cdd6bc154e75acb4e416fa7e63d8055a508c8e
liuyiyang <liuyiyang@co-mall.com>
2018-02-08 18:04:54 +0800

    初始化

---
+++ src/main/java/pers/lyy/fc/product/application/ProductService.java
@@ -0,0 +1,14 @@
+package pers.lyy.fc.product.application;
+
+/**
+ * <p>简介</p>
+ *
+ * @author liuyiyang Email:liuyiyang@co-mall.com
+ * @version 2.1.0
+ * @since 2018年02月08日 下午5:44
+ */
+public class ProductService {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
